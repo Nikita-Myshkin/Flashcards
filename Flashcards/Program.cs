286d6d6f6f38a810537c95b6b84b4e63722bc7f6
Nikita Myshkin <n_myshkin@icloud.com>
2025-10-05 14:41:51 +0300

    Add save and load data

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,35 +1,18 @@
 ﻿using Flashcards;
 
-var begginnersList = new WordList
-{
-    Name = "For begginners"
-};
+var manager = new DataManager("wordslist.json");
 
-var card1 = new Flashcard()
+var list = new WordList { Name = "My words!"};
+list.Cards.Add(new Flashcard
 {
-    Word = "blame",
-    Definition = "to say or think that a person or thing is responsible for something bad",
+    Word = "test",
+    Definition = "check something",
     Status = LearningStatus.Learning
-};
-card1.Examples.Add("Don't blame me for your mistakes.");
+});
 
-var card2 = new Flashcard()
-{
-    Word = "camera",
-    Definition = "a device for taking photographs",
-    Status = LearningStatus.Learned
-};
-card2.Examples.Add("I bought a new camera yesterday.");
+manager.SaveWordList(list);
+Console.WriteLine("Saved!");
 
-begginnersList.Cards.Add(card1);
-begginnersList.Cards.Add(card2);
-
-Console.WriteLine($"List:  {begginnersList.Name}");
-Console.WriteLine($"Created: {begginnersList.CreatedDate}");
-Console.WriteLine($"Total words: {begginnersList.WordCount}");
-Console.WriteLine("\nWords:");
-
-foreach (var card in begginnersList.Cards)
-{
-    Console.WriteLine($" - {card.Word} {(card.Status)}");
-}
+var loaded = manager.LoadWordList();
+Console.WriteLine($"Loaded: {loaded?.Name}");
+Console.WriteLine($"Words: {loaded?.WordCount}");
